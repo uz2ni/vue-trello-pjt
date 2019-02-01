fccd59f961cb2c1110cfceacfef235bb92bda5e2
yujakong <uzini_@naver.com>
2019-02-01 21:30:22 +0900

    순수 자바스크립트 request

--- src/Components/Home.vue
+++ src/Components/Home.vue
@@ -3,6 +3,10 @@
     Home
     <div>
       Board List:
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        API result: {{ apiRes }}
+      </div>
       <ul>
         <li><router-link to="/b/1">Board 1</router-link></li>
         <li><router-link to="/b/2">Board 2</router-link></li>
@@ -13,6 +17,34 @@
 
 <script>
 export default {
+  data() {
+    return {
+      loading: false,
+      apiRes: ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      const req = new XMLHttpRequest()
+
+      req.open('GET', 'http://localhost:3000/health')
+
+      req.send()
+
+      req.addEventListener('load', () => {
+        this.loading = false
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response: JSON.parse(req.response)
+        }
+      })
+    }
+  }
 }
 </script>
 
